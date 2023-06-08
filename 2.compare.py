31421decead41cd40e0d8989a2258acc79348603
IvanaXu <1440420407@qq.com>
2023-06-08 20:36:19 +0800

    update Readme & views

--- 2.compare.py
+++ 2.compare.py
@@ -102,7 +102,7 @@ with open("README.md", "w") as f:
     # idf["Rank"] = idf["UP%"].rank(ascending=True).apply(lambda x: int(x) * "+")
     
     TOP = 10
-    idf["Progress"] = idf["UP%"].apply(lambda x: int(float(x[:-1])/100 * TOP) * "+")
+    idf["Progress"] = idf["UP%"].apply(lambda x: int(float(x[:-1])/100 * TOP) * ">")
     idf["rank"] = idf["version"].apply(fTASK)
     idf.sort_values("rank", inplace=True)
     print(idf)
