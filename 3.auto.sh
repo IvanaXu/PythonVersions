f545f4dd39a1c458b4d701b892f09629c60f25bc
IvanaXu <ivan@tangjt.cn>
2024-06-24 15:57:09 +0800

    Update 3.auto.sh

--- 3.auto.sh
+++ 3.auto.sh
@@ -2,4 +2,5 @@
 pip3 install tqdm pandas tabulate
 git config --global user.email "ivan@tangjt.cn"
 git config --global user.name "ivan"
+
 # watch -n 2 -d "docker stats --no-stream && tail -n 20 py3.12.3.log"
